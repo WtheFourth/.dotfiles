1685ddf6ac8b5ad4aaf0473ee092f7a6be983e2c
Walter Kennedy <walter.kennedy@hudl.com>
2025-03-13 11:44:49 -0500

    working on telescope

---
+++ .config/nvim/lua/plugins/completions.lua
@@ -0,0 +1,11 @@
+return {
+  
+  "hrsh7th/nvim-cmp",
+  opts = {
+    performance = {
+      debounce = 0, -- default is 60ms
+      throttle = 0, -- default is 30ms
+    },
+  }
+
+}
--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -2,8 +2,19 @@ return {
     'nvim-telescope/telescope.nvim', tag = '0.1.8',
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
+      local telescope = require('telescope')
       local builtin = require('telescope.builtin')
-      vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+      telescope.setup({
+        defaults = {
+          file_ignore_patterns = { '^.git/' }
+        },
+        pickers = {
+          find_files = {
+            hidden = true
+          }
+        }
+      })
+      vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = 'Telescope find files' })
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
       vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
       vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
