fc5bd573b8263e551b2d0ad3e1d64333e5fb82ae
Walter Kennedy <walter.kennedy@hudl.com>
2025-03-20 09:10:21 -0500

    update various nvim keymaps etc

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -35,10 +35,13 @@ require("config.lazy")
 
 --#region Telescope
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+vim.keymap.set('n', '<leader>ft', builtin.treesitter, { desc = 'Telescope treesitter' })
+vim.keymap.set('n', '<leader>fk', builtin.keymaps, { desc = 'Telescope keymaps' })
+vim.keymap.set('n', '<leader>fl', builtin.lsp_references, { desc = 'Telescope LSPs' })
 --#endregion
 
 --#region LSPs
