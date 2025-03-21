0eb937c7745bed08ee89b14642d5c3c2274d84e4
Walter Kennedy <walter.kennedy@hudl.com>
2025-03-20 20:05:47 -0500

    add lsplines, work on quickfix

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,52 +1,61 @@
 -- checking if we're using WSL, setting up clipboard appropriately
 local function is_wsl()
-  local sys = vim.fn.system('uname -r');
-  return sys:lower():match('wsl') ~= nil
+	local sys = vim.fn.system("uname -r")
+	return sys:lower():match("wsl") ~= nil
 end
 
 if is_wsl() then
-  vim.g.clipboard = {
-    name = 'WslClipboard',
-    copy = {
-      ['+'] = 'clip.exe',
-      ['*'] = 'clip.exe',
-    },
-    paste = {
-      ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-      ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-    },
-    cache_enabled = 0,
-  }
+	vim.g.clipboard = {
+		name = "WslClipboard",
+		copy = {
+			["+"] = "clip.exe",
+			["*"] = "clip.exe",
+		},
+		paste = {
+			["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+			["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+		},
+		cache_enabled = 0,
+	}
 end
 
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.shiftwidth = 2
 vim.o.expandtab = true
-vim.o.clipboard = 'unnamedplus'
+vim.o.clipboard = "unnamedplus"
 vim.o.showmode = false
-vim.o.inccommand = 'split'
+vim.o.inccommand = "split"
 vim.g.have_nerd_font = true
 vim.o.cursorline = true
 
 require("config.lazy")
 
 --#region Keymaps
+--#region Builtin
+vim.keymap.set("n", "<leader>cc", "<cmd>copen<CR>", { desc = "Open Quickfix window" })
+vim.keymap.set("n", "<leader>ce", "<cmd>cclose<CR>", { desc = "Close Quickfix window" })
 
 --#region Telescope
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-vim.keymap.set('n', '<leader>ft', builtin.treesitter, { desc = 'Telescope treesitter' })
-vim.keymap.set('n', '<leader>fk', builtin.keymaps, { desc = 'Telescope keymaps' })
-vim.keymap.set('n', '<leader>fl', builtin.lsp_references, { desc = 'Telescope LSPs' })
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<C-p>", builtin.git_files, { desc = "Telescope find files" })
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
+vim.keymap.set("n", "<leader>ft", builtin.treesitter, { desc = "Telescope treesitter" })
+vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "Telescope keymaps" })
+vim.keymap.set("n", "<leader>fl", builtin.lsp_references, { desc = "Telescope LSPs" })
 --#endregion
 
 --#region LSPs
-vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action)
-vim.keymap.set('n', '<leader>cf', vim.lsp.buf.format)
+vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action)
+vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format)
 --#endregion
 
 --#endregion
+
+vim.api.nvim_create_autocmd("DiagnosticChanged", {
+	callback = function()
+		vim.diagnostic.setqflist({ open = false })
+	end,
+})
