89c76e047d2269eee8afc96cdb9e4b66607fa9a5
Walter Kennedy <walter.kennedy@hudl.com>
2025-03-21 14:09:41 -0500

    update keymaps

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -46,29 +46,9 @@ vim.o.cursorline = true
 require("config.lazy")
 
 --#region Keymaps
---#region Quickfix
 vim.keymap.set("n", "<leader>cc", toggle_qf, { desc = "Toggle Quickfix window" })
---#endregion
-
---#region Telescope
-local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<C-p>", builtin.git_files, { desc = "Telescope find files" })
-vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
-vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
-vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
-vim.keymap.set("n", "<leader>ft", builtin.treesitter, { desc = "Telescope treesitter" })
-vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "Telescope keymaps" })
-vim.keymap.set("n", "<leader>fl", builtin.lsp_references, { desc = "Telescope LSPs" })
---#endregion
-
---#region LSPs
-vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code action" })
-vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, { desc = "LSP format" })
---#endregion
-
 vim.keymap.set("n", "<c-j>", "<c-d>")
 vim.keymap.set("n", "<c-k>", "<c-u>")
-
 --#endregion
 
 vim.api.nvim_create_autocmd("DiagnosticChanged", {
--- nvim/.config/nvim/lua/plugins/lspconfigs.lua
+++ nvim/.config/nvim/lua/plugins/lspconfigs.lua
@@ -1,26 +1,32 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function() require("mason").setup() end
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "ts_ls", "ruby_lsp", "rubocop", "cssls", "eslint", "csharp_ls" }
-      })
-      require("mason-lspconfig").setup_handlers {
-        function(server_name)
-          require("lspconfig")[server_name].setup {}
-        end
-        -- server-specific handlers
-        -- ["server-name'] = function()
-        --   require("other-thing").setup {}
-        -- end
-      }
-    end
-  },
-  {
-    "neovim/nvim-lspconfig"
-  }
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "ts_ls", "ruby_lsp", "rubocop", "cssls", "eslint", "csharp_ls" },
+			})
+			require("mason-lspconfig").setup_handlers({
+				function(server_name)
+					require("lspconfig")[server_name].setup({})
+				end,
+				-- server-specific handlers
+				-- ["server-name'] = function()
+				--   require("other-thing").setup {}
+				-- end
+			})
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code action" })
+			vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, { desc = "LSP format" })
+			vim.keymap.set("n", "<leader>cd", vim.lsp.buf.definition, { desc = "Go to definition" })
+			vim.keymap.set("n", "<leader>cD", vim.lsp.buf.declaration, { desc = "Go to declaration" })
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+	},
 }
