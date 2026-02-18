5e4b34750ce34c63a86e814c108a418725694349
Walter Kennedy <walter.kennedy@hudl.com>
2026-02-18 11:33:03 -0600

    update nvim configs

--- nvim/.config/nvim/lua/plugins/lspconfigs.lua
+++ nvim/.config/nvim/lua/plugins/lspconfigs.lua
@@ -15,22 +15,16 @@ return {
 		end,
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "ts_ls", "ruby_lsp", "rubocop", "cssls", "eslint", "csharp_ls" },
-			})
-			require("mason-lspconfig").setup_handlers({
-				function(server_name)
-					require("lspconfig")[server_name].setup({})
-				end,
-				-- server-specific handlers
-				-- ["server-name'] = function()
-				--   require("other-thing").setup {}
-				-- end
+				ensure_installed = { "lua_ls", "ts_ls", "ruby_lsp", "rubocop", "cssls", "eslint", "omnisharp" },
+				automatic_installation = true,
 			})
 		end,
 	},
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
+			vim.lsp.enable({ "lua_ls", "ts_ls", "ruby_lsp", "rubocop", "cssls", "eslint", "omnisharp" })
+
 			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code action" })
 			vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, { desc = "LSP format" })
 			vim.keymap.set("n", "<leader>cr", vim.lsp.buf.references, { desc = "Show references" })
