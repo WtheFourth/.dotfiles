6fcf750aaf369e7c52ad6f182ee68c3ab0998c65
Walter Kennedy <walter.kennedy@hudl.com>
2026-03-11 11:27:16 -0500

    formatter formatting

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -5,20 +5,85 @@ return {
 		lazy = false,
 		keys = {
 			-- Picker
-			{ "<C-p>", function() require("snacks").picker.files() end, desc = "Find files" },
-			{ "<leader>ff", function() require("snacks").picker.git_files() end, desc = "Find git files" },
-			{ "<leader>fg", function() require("snacks").picker.grep() end, desc = "Live grep" },
-			{ "<leader>fb", function() require("snacks").picker.buffers() end, desc = "Buffers" },
-			{ "<leader>fh", function() require("snacks").picker.help() end, desc = "Help tags" },
-			{ "<leader>fk", function() require("snacks").picker.keymaps() end, desc = "Keymaps" },
-			{ "<leader>fr", function() require("snacks").picker.recent() end, desc = "Recent files" },
+			{
+				"<C-p>",
+				function()
+					require("snacks").picker.files()
+				end,
+				desc = "Find files",
+			},
+			{
+				"<leader>ff",
+				function()
+					require("snacks").picker.git_files()
+				end,
+				desc = "Find git files",
+			},
+			{
+				"<leader>fg",
+				function()
+					require("snacks").picker.grep()
+				end,
+				desc = "Live grep",
+			},
+			{
+				"<leader>fb",
+				function()
+					require("snacks").picker.buffers()
+				end,
+				desc = "Buffers",
+			},
+			{
+				"<leader>fh",
+				function()
+					require("snacks").picker.help()
+				end,
+				desc = "Help tags",
+			},
+			{
+				"<leader>fk",
+				function()
+					require("snacks").picker.keymaps()
+				end,
+				desc = "Keymaps",
+			},
+			{
+				"<leader>fr",
+				function()
+					require("snacks").picker.recent()
+				end,
+				desc = "Recent files",
+			},
 			-- Lazygit
-			{ "<leader>gg", function() require("snacks").lazygit() end, desc = "Lazygit" },
-			{ "<leader>gf", function() require("snacks").lazygit.log_file() end, desc = "Lazygit file history" },
-			{ "<leader>gl", function() require("snacks").lazygit.log() end, desc = "Lazygit log" },
+			{
+				"<leader>gg",
+				function()
+					require("snacks").lazygit()
+				end,
+				desc = "Lazygit",
+			},
+			{
+				"<leader>gf",
+				function()
+					require("snacks").lazygit.log_file()
+				end,
+				desc = "Lazygit file history",
+			},
+			{
+				"<leader>gl",
+				function()
+					require("snacks").lazygit.log()
+				end,
+				desc = "Lazygit log",
+			},
 		},
 		opts = {
-			picker = { enabled = true },
+			picker = {
+				enabled = true,
+				sources = {
+					files = { hidden = true },
+				},
+			},
 			dashboard = {
 				enabled = true,
 				preset = {
@@ -29,12 +94,37 @@ return {
 						"██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║",
 						"██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║",
 						"╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝",
-						"                                           NIGHTLY",
+						("                                           v%d.%d.%d"):format(
+							vim.version().major,
+							vim.version().minor,
+							vim.version().patch
+						),
 					}, "\n"),
 					keys = {
-						{ icon = " ", key = "f", desc = "Find file", action = function() require("snacks").picker.files() end },
-						{ icon = " ", key = "r", desc = "Recent files", action = function() require("snacks").picker.recent() end },
-						{ icon = " ", key = "g", desc = "Live grep", action = function() require("snacks").picker.grep() end },
+						{
+							icon = " ",
+							key = "f",
+							desc = "Find file",
+							action = function()
+								require("snacks").picker.files()
+							end,
+						},
+						{
+							icon = " ",
+							key = "r",
+							desc = "Recent files",
+							action = function()
+								require("snacks").picker.recent()
+							end,
+						},
+						{
+							icon = " ",
+							key = "g",
+							desc = "Live grep",
+							action = function()
+								require("snacks").picker.grep()
+							end,
+						},
 						{ icon = " ", key = "e", desc = "File browser", action = ":Oil" },
 						{ icon = " ", key = "q", desc = "Quit", action = ":qa" },
 					},
