6ae4a40bffcd3f2dff3399ae3d756479910b00b9
Walter Kennedy <walter.kennedy@hudl.com>
2026-03-12 16:15:18 -0500

    Catppuccin

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -9,7 +9,34 @@ return {
 					highlights.CursorLineNr = { fg = colors.yellow }
 				end,
 			})
-			vim.cmd.colorscheme("tokyonight")
+			-- vim.cmd.colorscheme("tokyonight")
+		end,
+	},
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				integrations = {
+					blink_cmp = {
+						style = "bordered",
+					},
+					diffview = true,
+					flash = true,
+					mason = true,
+					snacks = {
+						enabled = true,
+					},
+					which_key = true,
+				},
+				custom_highlights = function(colors)
+					return {
+						CursorLineNr = { fg = colors.flamingo },
+					}
+				end,
+			})
+			vim.cmd.colorscheme("catppuccin-mocha")
 		end,
 	},
 }
